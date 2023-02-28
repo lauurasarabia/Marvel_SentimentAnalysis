553c60267f29c83c41a3015f43387d22b3c993f2
Laura Sarabia <lauura.sarabia97@gmail.com>
2023-02-28 17:50:52 +0100

    update

--- notebooks/python_files/cleaning_csv.py
+++ notebooks/python_files/cleaning_csv.py
@@ -13,10 +13,12 @@ from nltk.sentiment import SentimentIntensityAnalyzer
 from tqdm.notebook import tqdm
 
 def reading_csv(file):
+    """ Reading csv files as a DataFrame with pandas"""
     df = pd.read_csv(f"/Users/lauurasarabia/Ironhack/projects/project4/data/{file}.csv")
     return df
 
 def dropping_columns():
+    """Drop unnecessary columns from marvel df"""
     marvel.drop(['Adam McKay', 'Anna Boden', 
                  'Art Marcum', 'Ashley Edward Miller',
                  'Chris McKenna', 'Christopher Ford', 
@@ -42,6 +44,7 @@ def drop_rename():
     return marvel_sentiment
 
 def plotting_nltk():
+    """Create three different plots for being able to visualize the sentiment analysis (positive, neutral and negative)"""
     fig, axs = plt.subplots(1, 3, figsize=(16,4))
     sns.barplot(data=marvel_sentiment, x='year', y='pos', ax=axs[0])
     sns.barplot(data=marvel_sentiment, x='year', y='neu', ax=axs[1])
@@ -53,6 +56,7 @@ def plotting_nltk():
     return plt.show()
 
 def replacing_characters():
+    """Change manually character names and removing the spaces at the beginning of character's name for matching both DataFrames"""
     top_characters["character"].replace({' IRON MAN':'TONY STARK',
                                      ' CAPTAIN AMERICA':'STEVE ROGERS',
                                      ' SPIDER MAN':'PETER PARKER',
@@ -71,11 +75,10 @@ def replacing_characters():
     return top_characters
 
 def cleaning_df():
+    """Drop unnecessary columns and replacing special characters for underscores"""
     movie_top_characters.drop(['Unnamed: 0', '0'], axis=1, inplace=True)
     movie_top_characters.rename(columns=lambda x: x.strip().replace(" ", "_"), inplace=True)
     movie_top_characters.rename(columns=lambda x: x.strip().replace(":", ""), inplace=True)
     movie_top_characters.rename({"character":"full_name"}, axis=1, inplace=True)
     return movie_top_characters
 
-def to_csv(file):
-    f"{file}".to_csv(f'/Users/lauurasarabia/Ironhack/projects/project4/data/{file}.csv')
--- notebooks/python_files/sql_queries.py
+++ notebooks/python_files/sql_queries.py
@@ -5,6 +5,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+import python_files.cleaning_csv as cleaning
 
 plt.style.use('ggplot')
 
@@ -12,7 +13,11 @@ import nltk
 from nltk.sentiment import SentimentIntensityAnalyzer
 from tqdm.notebook import tqdm
 
+load_dotenv()
+
 def connection_sql (password_sql, dbName):
+    password_sql = os.getenv("password_sql")
+    dbName = "marvel"
     connectionData = f"mysql+pymysql://root:{password_sql}@localhost/{dbName}"
     engine = alch.create_engine(connectionData)
     return engine
